6d02825dffe7c811091fe68c538f2fa23674332a
ReanaJohnson <Reana.Johnson@cdpipelinedevshop.com>
2022-07-28 12:40:10 +0000

    First commit

---
+++ fbdemo/src/App.js
@@ -0,0 +1,40 @@
+import { Button, Typography } from '@mui/material';
+import {Add, Settings } from '@mui/icons-material';
+function App() {
+  return (
+    <div>
+     <Button variant="text">Text</Button>
+     <Button 
+     startIcon={<Settings />}
+      variant="contained"
+       color="secondary"
+        size="small"
+        >
+          Settings
+          </Button>
+     <Button
+      startIcon={<Add />}
+       variant="contained"
+        color="success"
+         size="small"
+         >
+          Add new post
+          </Button>
+     <Button variant="outlined" disabled>Outlined
+     </Button>
+     <Typography variant="h1" component="p">
+  It uses h1 style but it's a p tag
+</Typography>
+<Button variant="contained" sx={{
+  backgroundColor:"skyblue",
+  color:"#888",
+  margin:5,
+  "&:hover":{
+    backgroundColor:"lightblue"
+  }
+}}>My Unique Button </Button> 
+    </div>
+  );
+}
+
+export default App;
